5d2ba99789b1d6a16c8f557f104762951597a63c
jagraves21 <jagraves21@students.tntech.edu>
2017-08-08 16:03:48 -0400

    Added a circle spanning tree graph generator.

--- python/create_circle_graph.py
+++ python/create_circle_graph.py
@@ -1,14 +1,14 @@
 import argparse
 import networkx
-from generators import generate_geometric_circle_graph
+from generators import generate_circle_graph
 from graph_utils import write_graph
 
 def main(vertex_count, neighbors, distance, undirected, append_attributes, output_file, output_type):
 	
 	if undirected:
-		graph = generate_geometric_circle_graph(networkx.Graph(), vertex_count, neighbors, distance)
+		graph = generate_circle_graph(networkx.Graph(), vertex_count, neighbors, distance)
 	else:
-		graph = generate_geometric_circle_graph(networkx.DiGraph(), vertex_count, neighbors, distance)
+		graph = generate_circle_graph(networkx.DiGraph(), vertex_count, neighbors, distance)
 	
 	if not output_file:
 		output_file = "/dev/stdout"
@@ -23,9 +23,9 @@ if __name__ == "__main__":
 	parser.add_argument("-o",  "--output-file",               metavar="outfile", help="output graph name, standard out by default")
 	parser.add_argument("-ot", "--output-type", default=None, metavar="type",    help="output type")
 	
-	parser.add_argument("-v", "--vertex-count", type=int,   default=100,  metavar="n", help="number of vertices")
-	parser.add_argument("-n", "--neighbors",    type=int,   default=5,    metavar="n", help="number of neighbors")
-	parser.add_argument("-d", "--distance",     type=float, default=None, metavar="n", help="number of neighbors")
+	parser.add_argument("-v", "--vertex-count", type=int,   default=100,  metavar="N", help="number of vertices")
+	parser.add_argument("-n", "--neighbors",    type=int,   default=5,    metavar="N", help="number of neighbors")
+	parser.add_argument("-d", "--distance",     type=float, default=None, metavar="N", help="number of neighbors")
 	
 	parser.add_argument("-u", "--undirected", action="store_true", help="construct an undirected graph, directed by default")
 	parser.add_argument("-a", "--append-attributes", action="store_true", help="write graph attributes to RDF file")
--- python/generators.py
+++ python/generators.py
@@ -1,6 +1,7 @@
 import numpy
 from sklearn.neighbors import BallTree
 import networkx
+import itertools
 
 def generate_points(count):
 	from math import pi
@@ -37,8 +38,27 @@ def add_knn_edges(graph, kk, distance):
 			if not distance or distance >= distances[ii][1]:
 				graph.add_edge(src,dst)
 
-def generate_geometric_circle_graph(graph=networkx.Graph(), vertex_count=100, neighbors=1, distance=1.0):
+def generate_circle_graph(graph=networkx.Graph(), vertex_count=100, neighbors=1, distance=1.0):
 	generate_circular_vertices(graph, vertex_count)
 	add_knn_edges(graph, neighbors+1, distance)
 	return graph
 
+def add_spanning_tree_edges(graph):
+	complete_graph = networkx.complete_graph(networkx.number_of_nodes(graph))
+	print graph
+
+	def generate_edges(graph):
+		pos = networkx.get_node_attributes(graph, "pos")
+		for src,dst in itertools.combinations(range(networkx.number_of_nodes(graph)), 2):
+			distance = (pos[src][0] - pos[dst][0])**2 + (pos[src][1] - pos[dst][1])**2
+			yield (src,dst,distance)
+	
+	complete_graph.add_weighted_edges_from(generate_edges(graph))
+	tree = networkx.minimum_spanning_tree(complete_graph)
+	
+	graph.add_edges_from(tree.edges())
+
+def generate_circle_spanning_tree_graph(graph=networkx.Graph(), vertex_count=100):
+	generate_circular_vertices(graph, vertex_count)
+	add_spanning_tree_edges(graph)
+	return graph
